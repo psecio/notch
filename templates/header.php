505c0b2c03dc34675c9d168549dbce260f76853e
Chris Cornutt <enygma@phpdeveloper.org>
2014-12-20 13:46:35 -0600

    adding root link to site title, checking for logged in on top links

--- templates/header.php
+++ templates/header.php
@@ -13,12 +13,18 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Notch</a>
+					<a class="navbar-brand" href="/">Notch</a>
 					</div>
 					<div id="navbar" class="collapse navbar-collapse">
 					<ul class="nav navbar-nav">
 					<li class="active"><a href="/">Home</a></li>
-					<li class="active"><a href="/user/login">Login</a></li>
+
+					<?php if (isset($_SESSION['username'])): ?>
+						<li class="active"><a href="/user/logout">Logout</a></li>
+						<li class="active"><a href="/post/add">Add Post</a></li>
+					<?php else: ?>
+						<li class="active"><a href="/user/login">Login</a></li>
+					<?php endif; ?>
 					</ul>
 				</div><!--/.nav-collapse -->
 			</div>
