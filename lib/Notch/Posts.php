b152ddde327663bc35ddce6bf591e3a8e64d6f2a
Chris Cornutt <enygma@phpdeveloper.org>
2014-12-20 08:05:19 -0600

    updating Posts to use Base class

--- lib/Notch/Posts.php
+++ lib/Notch/Posts.php
@@ -2,28 +2,19 @@
 
 namespace Notch;
 
-class Posts
+class Posts extends Base
 {
-    protected $di;
-
-    public function __construct(\Pimple\Container $di)
-    {
-        $this->di = $di;
-    }
-
     public function getLatest($limit = 10)
     {
-        $db = $this->di['db'];
         $sql = 'select * from posts order by created desc limit '.$limit;
-        $posts = $db->fetch($sql);
+        $posts = $this->getDb()->fetch($sql);
 
         return $posts;
     }
 
     public function getDetail($postId)
     {
-        $db = $this->di['db'];
         $sql = 'select * from posts where ID = '.$postId;
-        return $db->fetchOne($sql);
+        return $this->getDb()->fetchOne($sql);
     }
 }
