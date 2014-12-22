1b1b0f4f8c5aee1cac03fb18d27a7d6735482470
Chris Cornutt <chris.cornutt@hp.com>
2014-12-22 10:30:45 -0600

    lots of new routes added for post editing and commenting updates

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $di['db'] = function()
 };
 
 $app = new Slim\Slim(array(
-    'debug' => false
+    'debug' => true
 ));
 $app->error(function (\Exception $e) use ($app) {
     // do nothing...
@@ -37,9 +37,11 @@ $app->get('/', function() use ($app, $di) {
     $post = new Notch\Posts($di);
     $postList = $post->getLatest(10);
 
-    var_export($postList);
+    $data = array(
+        'posts' => $postList
+    );
 
-	$app->render('index/index.php', array('test' => 'foo'));
+	$app->render('index/index.php', $data);
 });
 
 /**
@@ -47,6 +49,7 @@ $app->get('/', function() use ($app, $di) {
  */
 $app->group('/post', function() use ($app, $di) {
 
+    // Post main route
     $app->get('/', function() use ($app, $di) {
         $app->render('post/index.php');
     });
@@ -54,12 +57,27 @@ $app->group('/post', function() use ($app, $di) {
     // Hint [Auth]: Protection?
     $app->get('/detail/:id', function($postId) use ($app, $di) {
         $post = new Notch\Posts($di);
+        $postData = $post->getDetail($postId);
+        $success = true;
+
+        $user = new Notch\Users($di);
+        $userData = $user->getUserByUsername($postData['author']);
+
+        if ($postData == null) {
+            $success = false;
+        }
+
         $data = array(
-            'detail' => $post->getDetail($postId),
-            'id' => $postId
+            'post' => $postData,
+            'success' => $success,
+            'id' => $postId,
+            'user' => $userData,
+            'currentUser' => @$_SESSION['username']
         );
         $app->render('post/detail.php', $data);
     });
+
+    // Post/add routes/....
     $app->get('/add', function() use ($app, $di) {
         $app->render('post/add.php');
     });
@@ -80,6 +98,66 @@ $app->group('/post', function() use ($app, $di) {
             'success' => $success,
             'message' => $message
         );
+        $app->render('post/add.php', $data);
+    });
+
+    // Post delete routes
+    $app->get('/delete/:id', function($postId) use ($app, $di) {
+        $post = new Notch\Posts($di);
+        $postData = $post->getDetail($postId);
+        $data = array(
+            'post' => $postData
+        );
+        $app->render('post/delete.php', $data);
+    });
+    // Hint [Auth]: Protection?
+    $app->post('/delete/:id', function($postId) use ($app, $di) {
+        $post = new Notch\Posts($di);
+        $postData = $post->getDetail($postId);
+        $message = 'Post deleted successfully!';
+
+        $success = $post->delete($postId);
+
+        if ($success == false) {
+            $message = 'There was an error deleting the post!';
+        }
+
+        $data = array(
+            'post' => $postData,
+            'success' => $success,
+            'message' => $message
+        );
+
+        $app->render('post/delete.php', $data);
+    });
+
+    $app->get('/edit/:id', function($postId) use ($app, $di) {
+        $post = new Notch\Posts($di);
+        $data = array(
+            'postData' => $post->getDetail($postId)
+        );
+
+        $app->render('/post/add.php', $data);
+    });
+    $app->post('/edit/:id', function($postId) use ($app, $di) {
+        $posted = $app->request->post();
+        $posted['author'] = @$_SESSION['username'];
+        $posted['id'] = $postId;
+
+        $post = new Notch\Posts($di);
+        $success = $post->save($posted);
+        $message = 'Post saved successfully!';
+
+        if ($success == false) {
+            $message = 'There was an error editing the post!';
+        }
+
+        $data = array(
+            'postData' => $post->getDetail($postId),
+            'success' => $success,
+            'message' => $message
+        );
+
         $app->render('post/add.php', $data);
     });
 });
