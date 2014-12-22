accbf89143f7aca35107c7497f7a58459f2c7135
Chris Cornutt <chris.cornutt@hp.com>
2014-12-22 10:29:32 -0600

    adding post output to index page

--- templates/index/index.php
+++ templates/index/index.php
@@ -1,3 +1,12 @@
-index.php
-
-<?php echo $test; ?>
+<h2>Latest Posts</h2>
+<br/>
+<?php foreach ($posts as $post): ?>
+<div class="post">
+    <a class="title" href="/post/detail/<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a><br/>
+    <span class="date">
+        <?php echo $post['author']; ?> @ <?php echo $post['created']; ?>
+    </span>
+    <p class="content"><?php echo nl2br($post['content']); ?></p>
+</div>
+<br/>
+<?php endforeach; ?>
