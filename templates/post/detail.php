0df10758b2f9e5de22cf06339edda69fa5341039
Chris Cornutt <chris.cornutt@hp.com>
2014-12-22 10:30:25 -0600

    outputting actual post content in detail view now

--- templates/post/detail.php
+++ templates/post/detail.php
@@ -1,3 +1,17 @@
-post/detail.php
+<br/>
+<?php if ($success == true): ?>
+<div class="post">
+    <a class="title" href="/post/detail/<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a><br/>
+    <span class="date">
+        <?php echo $post['author']; ?> @ <?php echo $post['created']; ?>
+    </span>
+    <p class="content"><?php echo nl2br($post['content']); ?></p>
+</div>
+<?php else: ?>
+<div class="alert alert-danger" role="alert">Invalid post!</div>
+<?php endif; ?>
 
-<?php echo $id; ?>
+<?php if ($post['author'] === $currentUser): ?>
+    <a href="/post/delete/<?php echo $post['id']; ?>">Delete</a><br/>
+    <a href="/post/edit/<?php echo $post['id']; ?>">Edit</a>
+<?php endif; ?>
