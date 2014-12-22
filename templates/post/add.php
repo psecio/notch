9f6a065f4890f34807027818a5812fafe6ad75c3
Chris Cornutt <chris.cornutt@hp.com>
2014-12-22 10:29:58 -0600

    updating add post view to handle edit too

--- templates/post/add.php
+++ templates/post/add.php
@@ -1,21 +1,33 @@
-<h2>Add New Post</h2>
+<h2><?php echo isset($postData) ? 'Edit' : 'Add New'; ?> Post</h2>
 
-<form class="form-horizontal" role="form" method="POST" action="/post/add">
+<?php if(isset($success)):
+    $type = ($success === false) ? 'alert-danger' : 'alert-success';
+    ?>
+    <div class="alert <?php echo $type; ?>" role="alert"><?php echo $message; ?></div>
+<?php endif;
+
+$url = (isset($postData)) ? '/post/edit/'.$postData['id'] : '/post/add';
+?>
+
+<form class="form-horizontal" role="form" method="POST" action="<?php echo $url; ?>">
     <div class="form-group">
         <label for="title" class="control-label col-sm-2">Title</label>
         <div class="col-sm-4">
-            <input type="text" class="form-control" id="title" name="title" placeholder="Title"/>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Title"
+                value="<?php if (isset($postData['title'])) { echo $postData['title']; } ?>"/>
         </div>
     </div>
     <div class="form-group">
         <label for="content" class="control-label col-sm-2">Content</label>
         <div class="col-sm-6">
-        	<textarea name="content" id="content" class="form-control" rows="10" placeholder="Content..."></textarea>
+        	<textarea name="content" id="content" class="form-control" rows="10" placeholder="Content..."><?php if (isset($postData['content'])) { echo $postData['content']; } ?></textarea>
         </div>
     </div>
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn">Post</button>
+            <button type="submit" class="btn">
+                <?php echo (isset($postData)) ? 'Update' : 'Post'; ?>
+            </button>
         </div>
     </div>
 </form>
