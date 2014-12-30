10e86010e8daf5b583a1e5c05e0f34ff6f6ab320
Chris Cornutt <enygma@phpdeveloper.org>
2014-12-30 09:25:48 -0600

    fixing User and Posts classes to work with PDO database adapter and bound params

--- lib/Notch/Posts.php
+++ lib/Notch/Posts.php
@@ -14,29 +14,47 @@ class Posts extends Base
 
     public function getDetail($postId)
     {
-        $sql = 'select * from posts where ID = '.$postId;
-        return $this->getDb()->fetchOne($sql);
+        $sql = 'select * from posts where ID = :postId';
+        $data = array(':postId' => $postId);
+
+        return $this->getDb()->fetchOne($sql, $data);
     }
 
     public function create($data)
     {
         $sql = 'insert into posts (title, content, author, created, updated)'
-            .' values ("'.$data['title'].'", "'.addslashes($data['content']).'", "'.$data['author'].'",'
-            .' now(), now())';
-        return $this->getDb()->execute($sql);
+            .' values (:title, :content, :author, now(), now())';
+        $data = array(
+            ':title' => $data['title'],
+            ':content' => $data['content'],
+            ':author' => $data['author']
+        );
+
+        return $this->getDb()->execute($sql, $data);
     }
 
     public function delete($postId)
     {
-        $sql = 'delete from posts where ID = '.$postId;
-        return $this->getDb()->execute($sql);
+        $sql = 'delete from posts where ID = :postId';
+        $data = array('postId' => $postId);
+
+        return $this->getDb()->execute($sql, $data);
     }
 
     public function save($data)
     {
-        $sql = 'update posts set title="'.$data['title'].'", content="'.addslashes($data['content']).'"'
-            .', author = "'.$data['author'].'",'
-            .' updated = now() where id = '. $data['id'];
-        return $this->getDb()->execute($sql);
+        if (!isset($data['id']) || !is_numeric($data['id'])) {
+            throw new \Exception('Invalid post ID!');
+        }
+
+        $sql = 'update posts set title = :title, content = :content, author = :author, updated = now()'
+            .' where id = '.$data['id'];
+        $data = array(
+            ':title' => $data['title'],
+            ':content' => $data['content'],
+            ':author' => $data['author']
+        );
+
+        return $this->getDb()->execute($sql, $data);
     }
 }
--- lib/Notch/Users.php
+++ lib/Notch/Users.php
@@ -6,27 +6,41 @@ class Users extends Base
 {
     public function getUserById($userId)
     {
-        return $this->getDb()->fetchOne('select * from users where id = '.$userId);
+        return $this->getDb()->fetchOne(
+            'select * from users where id = :userId',
+            array(':userId' => $userId)
+        );
     }
 
     public function getUserByUsername($username)
     {
-        return $this->getDb()->fetchOne('select * from users where username = "'.$username.'"');
+        return $this->getDb()->fetchOne(
+            'select * from users where username = :username',
+            array(':username' => $username)
+        );
     }
 
     public function login($username, $password)
     {
-        $sql = 'select * from users where username = "'.$username.'" and password = "'.$password.'"';
-        $result = $this->getDb()->fetchOne($sql);
+        $sql = 'select * from users where username = :username and password = :password';
+        $data = array(
+            ':username' => $username,
+            ':password' => $password
+        );
+        $result = $this->getDb()->fetchOne($sql, $data);
         return (empty($result)) ? false : true;
     }
 
     public function create($data)
     {
         $sql = 'insert into users (username, password, email, created, updated)'
-            .' values ("'.$data['username'].'", "'.$data['password'].'",'
-            .' "'.$data['email'].'", now(), now())';
+            .' values (:username, :password, :email, now(), now())';
+        $data = array(
+            ':username' => $data['username'],
+            ':password' => $data['password'],
+            ':email' => $data['email']
+        );
 
-        return $this->getDb()->execute($sql);
+        return $this->getDb()->execute($sql, $data);
     }
 }
