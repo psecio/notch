869f6d60e764861352396f341bf3e6973c348ab1
Chris Cornutt <enygma@phpdeveloper.org>
2015-01-03 07:25:07 -0600

    adding a link on the author name for posts

--- templates/index/index.php
+++ templates/index/index.php
@@ -4,7 +4,8 @@
 <div class="post">
     <a class="title" href="/post/detail/<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a><br/>
     <span class="date">
-        <?php echo $post['author']; ?> @ <?php echo $post['created']; ?>
+        <a href="/user/detail/<?php echo $post['author']; ?>"><?php echo $post['author']; ?></a>
+        @ <?php echo $post['created']; ?>
     </span>
     <p class="content"><?php echo nl2br($post['content']); ?></p>
 </div>
