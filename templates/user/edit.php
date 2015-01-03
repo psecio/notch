e1f8ce9de5b06277e226fa7db0931d82f477667b
Chris Cornutt <enygma@phpdeveloper.org>
2015-01-03 07:32:12 -0600

    updating button on user edit page

--- templates/user/edit.php
+++ templates/user/edit.php
@@ -30,7 +30,7 @@
     </div>
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn">Register</button>
+            <button type="submit" class="btn">Save</button>
         </div>
     </div>
 </form>
