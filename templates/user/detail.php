52c67aa30e7f9b4b4a95bdbfe5ad5c0ad31207f9
Chris Cornutt <enygma@phpdeveloper.org>
2015-01-18 07:30:55 -0600

    adding avatar code to user detail page

--- templates/user/detail.php
+++ templates/user/detail.php
@@ -2,17 +2,23 @@
 
 <table class="table">
 	<tr>
-		<td><b>Email:</b></td>
+		<td width="150"><b>Email:</b></td>
 		<td><?php echo $user['email']; ?></td>
 	</tr>
 	<tr>
-		<td><b>Created:</b></td>
+		<td width="150"><b>Created:</b></td>
 		<td><?php echo $user['created']; ?></td>
 	</tr>
 	<tr>
-		<td><b>Updated:</b></td>
+		<td width="150"><b>Updated:</b></td>
 		<td><?php echo $user['updated']; ?></td>
 	</tr>
+	<?php if (isset($user['avatar'])): ?>
+	<tr>
+		<td width="150"><b>Avatar:</b></td>
+		<td><img src="/assets/img/uploads/<?php echo $user['avatar']; ?>" width="150"/></td>
+	</tr>
+	<?php endif; ?>
 </table>
 
 <?php if ($user['username'] == $currentUser): ?>
